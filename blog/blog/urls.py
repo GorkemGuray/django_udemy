c75c616fc14ccac94eccf5de5eff62a40c131d81
Görkem Güray <grkmgry@gmail.com>
2020-04-27 00:52:58 +0300

    Ders-210
    User Uygulaması oluşturuldu

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('articles/',include('article.urls'))
+    path('articles/',include('article.urls')),
+    path('user/',include('user.urls'))
 
 ]
