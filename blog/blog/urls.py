c2b6fc93f0eab0540be0773016bca1169e48b2df
Görkem Güray <grkmgry@gmail.com>
2020-04-27 00:10:10 +0300

    Ders-208
    Dinamik URL Tanımlama

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('detail/<int:id>', views.detail, name='detail'),
 
 ]
